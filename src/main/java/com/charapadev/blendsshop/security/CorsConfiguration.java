07fd490148a290d10195288e739e72d5609f25f0
charapa <eduardocharapa@gmail.com>
2023-05-27 16:58:42 -0300

    fix: added cors configuration

---
+++ src/main/java/com/charapadev/blendsshop/security/CorsConfiguration.java
@@ -0,0 +1,26 @@
+package com.charapadev.blendsshop.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Configuration
+public class CorsConfiguration {
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        List<String> allowedMethods = Arrays.asList("POST", "GET", "PUT", "PATCH", "DELETE", "OPTIONS");
+
+        org.springframework.web.cors.CorsConfiguration configuration = new org.springframework.web.cors.CorsConfiguration().applyPermitDefaultValues();
+        configuration.setAllowedMethods(allowedMethods);
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+
+        return source;
+    }
+
+}
